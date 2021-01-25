f21bef09d7521aceb68c8c3f80a354feeac54ddf
Yan Pantoja <yanmpantoja@gmail.com>
2021-01-25 19:04:58 -0300

    feat: add create method with validate, data binding

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -7,11 +7,27 @@ use Livewire\Component;
 
 class ShowTweets extends Component
 {
-    public $message = 'Apenas um teste';
+    public $content = 'Apenas um teste';
+
+    protected $rules = [
+        'content' => 'required|min:3|max:255'
+    ];
 
     public function render()
     {
         $tweets = Tweet::with('user')->get();
         return view('livewire.show-tweets', compact('tweets'));
+    }
+
+    public function create()
+    {
+        $this->validate();
+
+        Tweet::create([
+            'content' => $this->content,
+            'user_id' => 1
+        ]);
+
+        $this->content = '';
     }
 }
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -2,10 +2,17 @@
     show
 
     <p>
-    {{ $message }}
+    {{ $content }}
     </p>
 
-    <input type="text" name="message" id="message" wire:model="message">
+    <form method="post" wire:submit.prevent="create">
+        <input type="text" name="content" id="content" wire:model="content">
+        @error('content')
+            {{ $message }}
+        @enderror
+        <button type="submit">Criar Tweet</button>
+    </form>
+
 
     <hr>
 
