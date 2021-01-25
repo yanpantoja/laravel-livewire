9794a0b7164acf152c950e92e4f1a3be847f66c7
Yan Pantoja <yanmpantoja@gmail.com>
2021-01-25 17:54:48 -0300

    feat: add show tweets

--- database/migrations/2021_01_25_180118_create_tweets_table.php
+++ database/migrations/2021_01_25_180118_create_tweets_table.php
@@ -16,7 +16,7 @@ class CreateTweetsTable extends Migration
         Schema::create('tweets', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->string('string');
+            $table->string('content');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users');
---
+++ resources/views/livewire/show-tweets.blade.php
@@ -0,0 +1,15 @@
+<div>
+    show
+
+    <p>
+    {{ $message }}
+    </p>
+
+    <input type="text" name="message" id="message" wire:model="message">
+
+    <hr>
+
+    @foreach ($tweets as $tweet)
+        {{ $tweet->user->name }} - {{ $tweet->content }} <br>
+    @endforeach
+</div>
